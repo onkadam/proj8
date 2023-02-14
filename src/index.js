15e8cb025f32cf7773100dde148630ee861a36d9
onkadam <onkark3737@gmail.com>
2023-02-14 12:27:10 +0530

    ok

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//Every component can have its own data/state
+class A extends React.Component {
+  //properties
+  state;
+
+  //constructer
+  constructor() {
+    super();
+    //State is the JS Object of the class which can hold the data
+    this.state = { name: "Praven", surname: "Tarate", address: "Beed" };
+  }
+
+  //methods
+  render() {
+    //Updating the existing state
+    this.setState({
+      ...this.state,
+      name:"Sandip"
+    })
+    return (
+      <div>
+        <h1>{this.state.name} {this.state.surname} {this.state.address}</h1>
+        <h1>{this.props.name} {this.props.surname} {this.props.children}</h1>
+      </div>
+    );
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <A name="Onkar" surname="Kadam">
+    Satara
+  </A>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
